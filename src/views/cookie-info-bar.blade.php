3fbef26ccbd363c4e9bfcfee916c23bb852c2bd8
Albert Gonzalez Pons <albertgp.dev@gmail.com>
2020-11-05 11:12:20 +0100

    add z-index

--- src/views/cookie-info-bar.blade.php
+++ src/views/cookie-info-bar.blade.php
@@ -1,5 +1,5 @@
 
-<div class="cookie-info-bar__content" style="position:absolute; bottom:0px; left:0px; width: 100%;@if(isset($_COOKIE[config('cookie-info-bar.cookie_name')])){{ 'display:none' }}@endif">
+<div class="cookie-info-bar__content" style="position:absolute; bottom:0px; left:0px; width: 100%; z-index:100;@if(isset($_COOKIE[config('cookie-info-bar.cookie_name')])){{ 'display:none' }}@endif">
     <div class="cookie-info-bar__message">
         <p>@lang('albertgpdev-cookie-info-bar::translations.main_text')</p>
     </div>    
